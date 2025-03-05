cf30452f9bc453310df2c98bc4730c7c5e1255dd
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-03-05 23:01:59 +0600

    crispy form added

--- first_app/forms.py
+++ first_app/forms.py
@@ -3,4 +3,13 @@ from django import forms
 class contactForm(forms.Form):
     name = forms.CharField(label='Username')
     email = forms.EmailField()
-    
+    age = forms.IntegerField()
+    weight = forms.FloatField()
+    balance = forms.DecimalField()
+    check = forms.BooleanField()
+    birthday = forms.DateField()
+    appointment = forms.DateTimeField()
+    CHOICES = [('S', 'Small'), ('M', 'Medium'), ('L', 'Large')]
+    size = forms.ChoiceField(choices=CHOICES)
+    PIZZA = [('P', 'Pepperoni'), ('M', 'Mushroom'), ('B', 'Beef')]
+    pizza = forms.MultipleChoiceField(choices=PIZZA)
