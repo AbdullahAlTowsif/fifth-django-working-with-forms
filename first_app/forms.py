69e8d5b06b84c4573accee751fed8288fdd8887b
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-03-06 01:24:24 +0600

    attributes and widgets

--- first_app/forms.py
+++ first_app/forms.py
@@ -1,17 +1,20 @@
 from django import forms
 
+# widgets = field to html input
+
 class contactForm(forms.Form):
-    name = forms.CharField(label='Username')
-    file = forms.FileField()
-    # email = forms.EmailField()
+    name = forms.CharField(label='Username', help_text="Text must be within 70 characters", required=False, disabled=False, widget=forms.Textarea(attrs={'placeholder' : 'Enter your Name'}))
+    # file = forms.FileField()
+    email = forms.EmailField()
     # age = forms.IntegerField()
     # weight = forms.FloatField()
     # balance = forms.DecimalField()
-    # check = forms.BooleanField()
-    # birthday = forms.DateField()
-    # appointment = forms.DateTimeField()
-    # CHOICES = [('S', 'Small'), ('M', 'Medium'), ('L', 'Large')]
-    # size = forms.ChoiceField(choices=CHOICES)
-    # PIZZA = [('P', 'Pepperoni'), ('M', 'Mushroom'), ('B', 'Beef')]
-    # pizza = forms.MultipleChoiceField(choices=PIZZA)
+    age = forms.CharField(widget=forms.NumberInput)
+    check = forms.BooleanField()
+    birthday = forms.DateField(widget=forms.DateInput(attrs={'type':'date'}))
+    appointment = forms.DateTimeField(widget=forms.DateInput(attrs={'type': 'datetime-local'}))
+    CHOICES = [('Small', 'Small'), ('Medium', 'Medium'), ('Large', 'Large')]
+    size = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
+    PIZZA = [('Pepperoni', 'Pepperoni'), ('Mushroom', 'Mushroom'), ('Beef', 'Beef')]
+    pizza = forms.MultipleChoiceField(choices=PIZZA, widget=forms.CheckboxSelectMultiple)
     
